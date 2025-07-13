a2d5520742ce7e9acc8f777e740a716ff58d9f97
krantamaki <kasper.rantamaki@gmail.com>
2025-07-13 13:47:40 +0300

    Moving risk management under pylib

---
+++ src/pylib/risk_management/__init__.py
@@ -0,0 +1,7 @@
+"""@package pylib.risk_management
+@author Kasper Rantamäki
+Submodule for risk management
+
+Submodule with functionality for basic risk management tasks like backtesting, simulation 
+or reporting risk measures
+"""
--- src/risk_management/__init__.py
+++
@@ -1,7 +0,0 @@
-"""@package risk_management
-@author Kasper Rantamäki
-Module for risk management
-
-Module with functionality for basic risk management tasks like backtesting, simulation 
-or reporting risk measures
-"""
