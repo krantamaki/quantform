b06c9363b9b657079e238b639ad2f9abdc61b4ed
krantamaki <kasper.rantamaki@gmail.com>
2025-07-13 17:07:51 +0300

    Adding the classes to init file

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -5,3 +5,10 @@ Module for database connections
 The module implements a DatabaseConnection abstract base class and a SQLiteConnection class which provides useful
 methods for database operators using the 'sqlite3' Python library.
 """
+
+
+__all__ = ["DatabaseConnection", "SQLiteConnection"]
+
+
+from .DatabaseConnection import DatabaseConnection
+from .SQLiteConnection import SQLiteConnection
