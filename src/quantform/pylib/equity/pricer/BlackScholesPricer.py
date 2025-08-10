6615b35326bf5a294b5301aceb437195ae257b6f
krantamaki <kasper.rantamaki@gmail.com>
2025-08-10 10:39:04 +0300

    Documentation added along with some fixes

--- src/quantform/pylib/equity/pricer/BlackScholesPricer.py
+++ src/quantform/pylib/equity/pricer/BlackScholesPricer.py
@@ -2,7 +2,7 @@
 @author Kasper Rantamäki
 Submodule with a very basic analytical Black-Scholes pricer
 """
-from typing import Callable, Optional, Literal
+from typing import Optional, Literal
 import numpy as np
 from scipy.stats import norm
 
@@ -15,13 +15,27 @@ class BlackScholesPricer(EquityPricerABC):
   """Option pricer based on the Black-Scholes model"""
   
   def __init__(self, maturity_date: QfDate, type: Literal["Call", "Put"], strike: float, risk_free_rate: float, volatility: Optional[float] = None, 
-               market_price: Optional[float] = None, underlying_value: Optional[float] = None,
-               report_date: Optional[QfDate] = None) -> None:
+               market_price: Optional[float] = None, underlying_value: Optional[float] = None, report_date: Optional[QfDate] = None) -> None:
+    """Constructor method
+    
+    Constructor method that stores the passed parameters as instance variables. Additionally, depending the passed parameters either
+    uses the passed value of 'volatility' as volatility or calculates the implied volatility based on the market price and the 
+    value of the underlying. If the implied volatility is to be calculated the kwargs 'market_price', 'underlying_value' and
+    'report_date' should be given. If 'volatility' parameter is given it takes precendence over calculating the implied volatility.
+    
+    @param maturity_date     The maturity date for the option
+    @param type              The type of option ('Call' or 'Put')
+    @param strike            The strike price of the option
+    @param risk_free_rate    The prevailing risk-free rate
+    @param volatility        The volatility of the underlying. Optional, defaults to None
+    @param market_price      The market price for the option. Optional, defaults to None
+    @param underlying_value  The value of the underlying for the market price. Optional, defaults to None
+    @param report_date       The date for the market price and the underlying value. Optional, defaults to None
+    @raises AssertionError   Raised if volatility is not given and cannot be computed
+    @return                  None
     """
-    TODO
-    """
-    assert (volatility is not None) or (market_price is not None and underlying_value is not None and report_date is not None), 
-    "Either volatility or market parameters need to be defined!"
+    
+    assert (volatility is not None) or (market_price is not None and underlying_value is not None and report_date is not None), "Either volatility or market parameters need to be defined!"
 
     self.__maturity_date = maturity_date
     self.__option_type   = type
@@ -31,9 +45,7 @@ class BlackScholesPricer(EquityPricerABC):
     
     
   def __call__(self, underlying_value: float, report_date: QfDate, vol: Optional[float] = None) -> float:
-    """
-    TODO
-    """
+    
     assert self.__maturity_date >= report_date, f"Report date must be at most maturity date! ({report_date} > {self.__maturity_date})"
   
     if self.__maturity_date == report_date: 
@@ -52,32 +64,29 @@ class BlackScholesPricer(EquityPricerABC):
            
   
   def delta(self, underlying_value: float, report_date: QfDate) -> float:
-    """
-    TODO
-    """
     return norm.cdf(self.d_plus(underlying_value, report_date))
   
   
   def vega(self, underlying_value: float, report_date: QfDate) -> float:
-    """
-    TODO
-    """
     return underlying_value * np.exp(-self.d_plus(underlying_value, report_date) ** 2 / 2) * \
            np.sqrt(report_date.timedelta(self.__maturity_date)) / np.sqrt(2 * np.pi)
             
   
   def gamma(self, underlying_value: float, report_date: QfDate) -> float:
-    """
-    TODO
-    """
     return self.__strike * discount(self.__rf, report_date.timedelta(self.__maturity_date)) * \
            norm.pdf(self.d_minus(underlying_value, report_date)) / (underlying_value ** 2 * self.__vol * \
            np.sqrt(report_date.timedelta(self.__maturity_date))
                                                
   
   def implied_volatility(self, market_price: float, underlying_value: float, report_date: QfDate) -> float:
-    """
-    TODO
+    """Method for calculating the implied volatility
+    
+    Method that calculates the volatility implicit in the option price
+    
+    @param market_price      The market price for the option
+    @param underlying_value  The value of the underlying for the market price
+    @param report_date       The date for the market price and the underlying value
+    @return                  The implied volatility
     """
     
     diff_func = self.__diff_factory(market_price, underlying_value, report_date)
@@ -97,28 +106,39 @@ class BlackScholesPricer(EquityPricerABC):
   
   
   def d_plus(self, underlying_value: float, report_date: QfDate, vol: float = None) -> float:
-    """
-    TODO
+    """The \f$d_+\f$ argument for the Black-Scholes formula
+    
+    @param underlying_value  The value of the underlying security
+    @param report_date       The valuation date
+    @param vol               The volatility of the underlying. Optional, defaults to None i.e. the instance variable is used for volatility
+    return                   The value of the \f$d_+\f$ argument
     """
     
     if vol is None:
-      return 1 / (self.__vol * np.sqrt(report_date.timedelta(self.__maturity_date))) *\
-             (np.log(underlying_value / self.__strike) + (self.__rf + self.__vol ** 2 / 2) *\
+      return 1 / (self.__vol * np.sqrt(report_date.timedelta(self.__maturity_date))) * \
+             (np.log(underlying_value / self.__strike) + (self.__rf + self.__vol ** 2 / 2) * \
              report_date.timedelta(self.__maturity_date))
     
-    else:
-      return 1 / (vol * np.sqrt(report_date.timedelta(self.__maturity_date))) *\
-             (np.log(underlying_value / self.__strike) + (self.__rf + vol ** 2 / 2) *\
-             report_date.timedelta(self.__maturity_date))
+    # Else
+    return 1 / (vol * np.sqrt(report_date.timedelta(self.__maturity_date))) * \
+           (np.log(underlying_value / self.__strike) + (self.__rf + vol ** 2 / 2) * \
+           report_date.timedelta(self.__maturity_date))
 
 
   def d_minus(self, underlying_value: float, report_date: datetime, vol: float = None) -> float:
+    """The \f$d_-\f$ argument for the Black-Scholes formula
+    
+    @param underlying_value  The value of the underlying security
+    @param report_date       The valuation date
+    @param vol               The volatility of the underlying. Optional, defaults to None i.e. the instance variable is used for volatility
+    return                   The value of the \f$d_-\f$ argument
+    """
     
     if vol is None:
-      return self.d_plus(underlying_value, report_date) -\
+      return self.d_plus(underlying_value, report_date) - \
              self.__vol * np.sqrt(report_date.timedelta(self.__maturity_date))
-    
-    else:
-      return self.d_plus(underlying_value, report_date, vol = vol) -\
-             vol * np.sqrt(report_date.timedelta(self.__maturity_date))
-  
+
+    # Else
+    return self.d_plus(underlying_value, report_date, vol = vol) - \
+           vol * np.sqrt(report_date.timedelta(self.__maturity_date))
+
--- src/quantform/pylib/equity/pricer/EquityPricerABC.py
+++ src/quantform/pylib/equity/pricer/EquityPricerABC.py
@@ -1,52 +1,83 @@
-"""
-TODO
+"""@package quantform.pylib.equity.pricer.EquityPricerABC
+@author Kasper Rantamäki
+Submodule with an abstract base class for equity derivative pricers
 """
 from abc import ABC, abstractmethod
-from typing import List, Dict, Optional, Callable
+from typing import List, Dict, Callable
 
 from ...QfDate import QfDate
 
 
 class EquityPricerABC(ABC):
-  """
-  TODO
-  """
+  """Abstract base class for equity derivative pricers"""
 
   @abstractmethod
-  def __call__(self, underlying_value: float, report_date: QfDate, vol; Optional[float] = None, *args: List[any], **kwargs: Dict[any]):
-    """
-    TODO
+  def __call__(self, underlying_value: float, report_date: QfDate, *args: List[any], **kwargs: Dict[any, any]):
+    """Call method
+    
+    Call method that calculates the price for the derivative given the value of the underlying and the valuation date.
+    
+    @param underlying_value  The value of the underlying security
+    @param report_date       The valuation date
+    @*args                   Additional class specific arguments
+    @**kwargs                Additional class specific keyword arguments
+    @return                  The value of the derivative
     """
     pass
   
 
   @abstractmethod
   def delta(self, underlying_value: float, report_date: QfDate, *args: List[any], **kwargs: Dict[any]) -> float:
-    """
-    TODO
+    """The delta of the derivative
+    
+    Calculates the delta (sensitivity to the value of the underlying) for the derivative
+    
+    @param underlying_value  The value of the underlying security
+    @param report_date       The valuation date
+    @*args                   Additional class specific arguments
+    @**kwargs                Additional class specific keyword arguments
+    @return                  The delta of the derivative
     """
     pass
   
 
   @abstractmethod
   def vega(self, underlying_value: float, report_date: QfDate, *args: List[any], **kwargs: Dict[any]) -> float:
-    """
-    TODO
+    """The vega of the derivative
+    
+    Calculates the vega (sensitivity to the volatility) for the derivative
+    
+    @param underlying_value  The value of the underlying security
+    @param report_date       The valuation date
+    @*args                   Additional class specific arguments
+    @**kwargs                Additional class specific keyword arguments
+    @return                  The vega of the derivative
     """
     pass
     
 
   @abstractmethod
   def gamma(self, underlying_value: float, report_date: QfDate, *args: List[any], **kwargs: Dict[any]) -> float:
-    """
-    TODO
+    """The gamma of the derivative
+    
+    Calculates the gamma (sensitivity to the delta) for the derivative
+    
+    @param underlying_value  The value of the underlying security
+    @param report_date       The valuation date
+    @*args                   Additional class specific arguments
+    @**kwargs                Additional class specific keyword arguments
+    @return                  The gamma of the derivative
     """
     pass
     
     
   def __diff_factory(self, market_price: float, underlying_value: float, report_date: QfDate) -> Callable:
+    """Method for generating a function that gives the difference between the model price and market price
+    
+    @param market_price      The market price for the derivative
+    @param underlying_value  The value of the underlying for the market price
+    @param report_date       The date for the market price and the underlying value
+    @return                  The difference function
     """
-    TODO
-    """
-    return lambda vol: self(underlying_value, report_date, vol = vol) - option_value
+    return lambda vol: self(underlying_value, report_date, vol = vol) - market_price
 
