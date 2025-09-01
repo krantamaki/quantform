a21fa087ea1ad8472a0ad52c08b209d176ac586a
krantamaki <kasper.rantamaki@gmail.com>
2025-09-01 19:07:34 +0300

    Adding the new classes to __init__ file

--- src/quantform/pylib/curve/__init__.py
+++ src/quantform/pylib/curve/__init__.py
@@ -4,11 +4,12 @@ Module for class implementations of various curves
 """
 
 
-__all__ = ["CurveABC", "GenericCurve", "ImpliedVolatilityCurve"]  # , "ProbabilityDensityCurve"]
+__all__ = ["CurveABC", "GenericCurve", "ImpliedVolatilityCurve", "ProbabilityDensityCurve", "DiscountCurve"]
 
 
 from .CurveABC import CurveABC
 from .GenericCurve import GenericCurve
 from .ImpliedVolatilityCurve import ImpliedVolatilityCurve
-# from .ProbabilityDensityCurve import ProbabilityDensityCurve
+from .ProbabilityDensityCurve import ProbabilityDensityCurve
+from .DiscountCurve import DiscountCurve
 
